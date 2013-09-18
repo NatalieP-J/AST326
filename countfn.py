77cbe391dbd995d46bb66cb285de67478162d4fb
NatalieP-J <natalie.pricejones@gmail.com>
2013-09-18 17:26:34 -0400

    Section 7

--- countfn.py
+++ countfn.py
@@ -28,6 +28,7 @@ def histogram(somearray,somename):
     plt.plot(hr,hist,drawstyle = 'steps-mid')
     plt.show()
     plt.savefig('{0}.png'.format(somename))
+    return hr,hist
 
 def plot_raw(somearray,units):
     plt.plot(somearray,drawstyle = 'steps-mid')
@@ -84,3 +85,75 @@ def pmt_loop (tsamp,nsamp,max_i):
             units = 'seconds'
         #plot_raw(raw_data[i],units) 
     return raw_data
+
+
+#Section 7
+def section7():
+    total_counts=[]
+    means = []
+    stds = []
+    variance = []
+    tsamp = [0.001,0.005,0.01,0.05,0.1,0.5]
+    nsamp = 100
+    time = timestamp()
+    for i in range(len(tsamp)):
+        name = 'section7-' + time + '-{0}-{1}'.format(nsamp,tsamp[i])
+        counts = get_counts(tsamp[i],nsamp)
+        print counts
+        save_data(name,counts)
+        mean = sum(counts)/len(counts)
+        means.append(mean)
+        vals = []
+        for j in range(len(counts)):
+            val = (counts[j]-mean)**2
+            vals.append(val)
+        std = np.sqrt(sum(vals)/(len(vals)-1))
+        stds.append(std)
+        var = std**2
+        variance.append(var)
+        total_counts.append(counts)
+    name = 'section7-' + time + '.png'
+    f = plt.figure()
+    plt.plot(means,variance,'-o')
+    x = np.arange(min(means),max(means))
+    y = x
+    plt.plot(x,y)
+    plt.savefig(name)
+    plt.show()
+    f = plt.figure()
+    name = 'section7-' + time + '-logplot.png'
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.plot(means,variance,'-o')
+    plt.plot(x,y,'-o')
+    plt.savefig(name)
+    plt.show()
+    return means,stds,variance,tsamp
+
+#Section 8 
+def poisson(x,mean):
+    newlist = []
+    for i in range(len(x)):
+        p = (mean**x[i]/x[i])*np.exp(-mean)
+        newlist.append(p)
+    return newlist
+
+def section8():
+    tsamp = 0.003
+    nsamp = 1000
+    time = timestamp()
+    name = 'section8-' + time + '-{0}-{1}'
+    counts = get_counts(tsamp,nsamp)
+    mean = sum(counts)/len(counts)
+    histogram = histogram(counts,name)
+    hr = histogram[0]
+    hist = histogram[1]
+    prob = []
+    total_hist = sum(hist)
+    for i in range(len(hist)):
+        p = hist[i]/total_hist
+        prob.append(p)
+    plt.figure()
+    plt.plot(hr,prob,'-o')
+    plt.plot(hr,poisson(hr,mean),'-o')
+    plt.show()
