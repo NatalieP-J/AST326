5523e5d79df6d1c98e8061273ada5e1c91f58d8c
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2013-09-20 11:23:27 -0400

    Continued work with Sections 8 and 9

--- countfn.py
+++ countfn.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+from scipy.misc import factorial
 
 def get_counts(tsamp,nsamp):
     counts = photoncount(tsamp,nsamp)
@@ -26,7 +27,6 @@ def histogram(somearray,somename):
     hr = np.arange(hmin,hmax+1)
     hist = np.array([np.where(somearray == i)[0].size for i in hr])
     plt.plot(hr,hist,drawstyle = 'steps-mid')
-    plt.show()
     plt.savefig('{0}.png'.format(somename))
     return hr,hist
 
@@ -88,7 +88,7 @@ def pmt_loop (tsamp,nsamp,max_i):
 
 
 #Section 7
-def section7():
+def section7new():
     total_counts=[]
     means = []
     stds = []
@@ -130,15 +130,56 @@ def section7():
     plt.show()
     return means,stds,variance,tsamp
 
-#Section 8 
+def section7(time):
+    total_counts=[]
+    means = []
+    stds = []
+    variance = []
+    tsamp = [0.001,0.005,0.01,0.05,0.1,0.5]
+    nsamp = 100
+    time = timestamp()
+    for i in range(len(tsamp)):
+        name = 'section7-' + time + '-{0}-{1}'.format(nsamp,tsamp[i])
+        counts = np.loadtxt(name+'.dat')
+        mean = sum(counts)/len(counts)
+        means.append(mean)
+        vals = []
+        for j in range(len(counts)):
+            val = (counts[j]-mean)**2
+            vals.append(val)
+        std = np.sqrt(sum(vals)/(len(vals)-1))
+        stds.append(std)
+        var = std**2
+        variance.append(var)
+        total_counts.append(counts)
+    name = 'section7-' + time + '.png'
+    f = plt.figure()
+    plt.plot(means,variance,'-o')
+    x = np.arange(min(means),max(means))
+    y = x
+    plt.plot(x,y)
+    plt.savefig(name)
+    plt.show()
+    f = plt.figure()
+    name = 'section7-' + time + '-logplot.png'
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.plot(means,variance,'-o')
+    plt.plot(x,y,'-o')
+    plt.savefig(name)
+    plt.show()
+    return means,stds,variance,tsamp
+
+#Section 8 - Poisson
 def poisson(x,mean):
     newlist = []
     for i in range(len(x)):
-        p = (mean**x[i]/x[i])*np.exp(-mean)
+        factx = int(factorial(x[i]))
+        p = (mean**x[i]/factx)*np.exp(-mean)
         newlist.append(p)
     return newlist
 
-def section8():
+def section8new():
     tsamp = 0.003
     nsamp = 1000
     time = timestamp()
@@ -151,9 +192,52 @@ def section8():
     prob = []
     total_hist = sum(hist)
     for i in range(len(hist)):
-        p = hist[i]/total_hist
+        p = float(hist[i])/total_hist
         prob.append(p)
     plt.figure()
     plt.plot(hr,prob,'-o')
     plt.plot(hr,poisson(hr,mean),'-o')
     plt.show()
+
+def section8(fname):
+    counts = np.loadtxt(fname+'.dat')
+    mean = sum(counts)/len(counts)
+    h = histogram(counts,name)
+    hr = h[0]
+    hist = h[1]
+    prob = []
+    total_hist = sum(hist)
+    for i in range(len(hist)):
+        p = float(hist[i])/total_hist
+        prob.append(p)
+    plt.figure()
+    plt.plot(hr,prob,'-o')
+    plt.plot(hr,poisson(hr,mean),'-o')
+    plt.show()
+    
+#Section 9
+def section9new(nmax):
+    moms = []
+    sdoms = []
+    means = []
+    stds = []
+    nsamps = []
+    time = timestamp()
+    for i in range(1,nmax+1):
+        n = 2**i
+        nsamps.append(n)
+    tsamp = 0.01
+    for j in range(len(nsamps)):
+        mean = []
+        std = []
+        for k in range(15):
+            name = 'section9-' + time + '-{0}-{1}_{2}'.format(tsamp,nsamp,k+1)
+            counts = get_counts(tsamp,nsamp[j])
+            save_data(name,counts)
+            mean = float(sum(counts))/len(counts)
+    
+
+
+    
+    
+    
